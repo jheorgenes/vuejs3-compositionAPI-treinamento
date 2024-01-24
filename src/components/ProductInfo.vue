8dcfdf8a08f0d090df94d7ac2411771c00c8e7ae
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-01-24 11:50:55 -0300

    - Aprendendo a usar Props para transferir dados de um componente pai para um componente filho

---
+++ src/components/ProductInfo.vue
@@ -0,0 +1,11 @@
+<template>
+Likes: {{ likes }}
+Comments: {{ comments }}
+</template>
+
+<script setup>
+defineProps({
+  likes: Number|String,
+  comments: Number|String
+})
+</script>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2 id="app-products">Products</h2>
+  <h2 id="my-app">Products my app</h2>
+
+  <h2>Product 1</h2>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi repellendus tempore odio voluptates natus corrupti modi mollitia, veniam omnis laudantium est porro ab? Laborum ipsum repellat nesciunt quibusdam culpa. Sunt.</p>
+
+  <ProductInfo :likes="product.likes" :comments="product.comments" />
+</template>
+
+<script setup>
+import ProductInfo from '@/components/ProductInfo.vue';
+import { reactive } from 'vue';
+
+const product = reactive({
+  likes: 5,
+  comments: 14
+})
+</script>
+
+<style scoped>
+#app-products {
+  color: red;
+}
+</style>
