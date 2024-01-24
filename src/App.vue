851827a95f264af5a9635b904a855d6ace2722d5
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-01-24 13:56:47 -0300

    - Aprendendo a enviar dados através de $emit dos componentes filhos para o pai

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,27 @@
   <Nav />
 
   <router-view></router-view>
+  <br>
+  <br>
+  <button @click="count++">add count - {{ count }}</button>
+
+  <!-- <CountChild @addCount="count++" /> -->
+  <!-- <CountChild @addCount="(value) => count+= value" /> -->
+  <CountChild @addCount="add" />
 </template>
 
 <script setup>
 import Nav from '@/components/Nav.vue';
+import CountChild from './components/CountChild.vue';
+import { ref } from 'vue'
+
+const count = ref(0);
+
+function add(value) {
+  count.value += value
+}
 
 </script>
 
 <style scoped>
-
 </style>
---
+++ src/components/CountChild.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- <button @click="$emit('addCount')">Add count from child component</button> -->
+  <button @click="add">Add count from child component</button>
+</template>
+
+<script setup>
+
+const emit = defineEmits(['addCount']); //Definindo propriedades que serão emitidas
+
+function add() {
+  emit('addCount', 10); //Emitindo para o componente pai
+}
+</script>
