e59f0b8a448e8a580d878d2354a033855c3632fd
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-01-25 16:16:02 -0300

    - Aprendendo a trabalhar com $refs no compositionAPI, embora para tratamento de listas seja melhor trabalhar com querySelector

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,46 @@
   <Nav />
 
   <router-view></router-view>
+
+  <img src="https://picsum.photos/200/300" alt="" id="img" ref="img">
+
+  <ul>
+    <li v-for="(product, index) in products" :key="index" ref="product">{{ product.name }}</li>
+  </ul>
 </template>
 
 <script setup>
 import Nav from '@/components/Nav.vue';
 import CountChild from './components/CountChild.vue';
-import { ref } from 'vue'
+import { ref, onMounted, reactive } from 'vue';
 
 const count = ref(0);
+
+const img = ref(null); //Declarando uma variável para receber $refs
+const product = ref([]);
 
 function add(value) {
   count.value += value
 }
+
+const products = reactive([
+  {
+    id: 1,
+    name: 'Imac'
+  },
+  {
+    id: 2,
+    name: 'Teclado'
+  }
+])
+
+onMounted(() => {
+  // console.log(document.querySelector('#img'));
+  // console.log(document.querySelectorAll('#product'));
+  // console.log(img.value);
+  console.log(product.value);
+})
+
 
 </script>
 
