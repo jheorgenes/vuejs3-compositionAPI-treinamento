8e82ad4252ed86c577f8901440cfff378822d2b3
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-01-26 17:16:28 -0300

    - Aprendendo a trabalhar com paginação com laravel - Requer instalação de componente para paginação. - Requer modificação no Laravel para trazer as informações de consulta na estrutura de paginação. - Implementação de scopo de slots para tradução de Anterior e Próxima

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,17 @@
   <input type="text" placeholder="Search" v-model="userSearch" @keyup="search">
 
   <ul>
-    <li v-for="(user, index) in users['users']" :key="index">{{ user.firstName }} {{ user.lastName }}</li>
+    <li v-for="(user, index) in users['users'].data" :key="index">{{ user.firstName }} {{ user.lastName }}</li>
   </ul>
+
+  <Bootstrap5Pagination :data="users['users']" @pagination-change-page="getUsers">
+    <template #prev-nav>
+      <span>&lt; Anterior</span>
+    </template>
+    <template #next-nav>
+      <span>Próximo &gt;</span>
+    </template>
+  </Bootstrap5Pagination>
 
   <div v-html="userNotFound"></div>
 </template>
@@ -13,6 +22,7 @@
 import http from '@/services/http';
 import _ from 'lodash';
 import { onMounted, ref, reactive, computed } from 'vue';
+import { Bootstrap5Pagination } from 'laravel-vue-pagination';
 
 const users = reactive({ users: [] });
 const userSearch = ref('');
@@ -22,14 +32,18 @@ const userNotFound = computed(() => {
   return (!loading.value && users['users'].length <= 0) ? '<span id="notFound">Nenhum usuário encontrado</span>' : '';
 });
 
-onMounted(async () => {
+async function getUsers(page = 1) {
   try {
-    const { data } = await http.get('/api/users');
+    const { data } = await http.get('/api/users?page='+Number(page));
     users['users'] = data;
     loading.value = false;
   } catch (error) {
     console.log(error.response.data);
   }
+}
+
+onMounted(() => {
+  getUsers();
 });
 
 const search = _.debounce(async () => {
