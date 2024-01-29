6519ea95be67d1c53d4941dcd8822a1f4418e5a2
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-01-29 11:31:13 -0300

    - Ajustando a pesquisa de usuários, quando a aplicação está utilizado paginação e configurando para retomar a lista paginada, quando não estiver efetuando nenhuma pesquisa.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
     <li v-for="(user, index) in users['users'].data" :key="index">{{ user.firstName }} {{ user.lastName }}</li>
   </ul>
 
-  <Bootstrap5Pagination :data="users['users']" @pagination-change-page="getUsers">
+  <Bootstrap5Pagination :data="users['users']" @pagination-change-page="handlePagination" :limit="2" :show-disabled="true"  size="default" align="center">
     <template #prev-nav>
       <span>&lt; Anterior</span>
     </template>
@@ -27,10 +27,15 @@ import { Bootstrap5Pagination } from 'laravel-vue-pagination';
 const users = reactive({ users: [] });
 const userSearch = ref('');
 const loading = ref(true);
+const searched = ref(false);
 
 const userNotFound = computed(() => {
-  return (!loading.value && users['users'].length <= 0) ? '<span id="notFound">Nenhum usuário encontrado</span>' : '';
+  return (!loading.value && users['users'].data.length <= 0) ? '<span id="notFound">Nenhum usuário encontrado</span>' : '';
 });
+
+function handlePagination(page) {
+  return searched.value ? searchUser(page) : getUsers(page);
+}
 
 async function getUsers(page = 1) {
   try {
@@ -46,19 +51,29 @@ onMounted(() => {
   getUsers();
 });
 
-const search = _.debounce(async () => {
+async function searchUser(page = 1) {
   try {
-    const { data } = await http.get('/api/users/search', {
+    const { data } = await http.get('/api/users/search?page='+Number(page), {
       params: {
         user: userSearch.value
       }
     });
 
-    console.log(data);
+    if(!userSearch.value) {
+      searched.value = false;
+      getUsers();
+      return;
+    }
+    
+    searched.value = true;
     users['users'] = data;
   } catch (error) {
     console.log(error.response.data);
   }
+}
+
+const search = _.debounce(async () => {
+  searchUser();
 }, 1000);
 </script>
 
